82f99a19ffb611ce5be7959b33d93fdd7f6c2a3a
Oryna Kasapova <oryna.kasapova@nure.ua>
2023-03-14 15:44:14 +0200

    Initial commit

---
+++ src/test/java/ua/nure/shoestore/ShoeStroreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoestrore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStroreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
