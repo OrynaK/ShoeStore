9717b7a89c1056a9a7b8df3ae94ff3c2796fe2c4
Klymenko <alexklimovv2@gmail.com>
2023-03-15 22:21:57 +0200

    срфтпуі еуіе

--- src/test/java/ua/nure/shoestore/ShoeStoreApplicationTests.java
+++ src/test/java/ua/nure/shoestore/ShoeStoreApplicationTests.java
@@ -1,10 +1,10 @@
-package ua.nure.shoestore;
+package com.example.shoestrore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoeStoreApplicationTests {
+class ShoeStroreApplicationTests {
 
     @Test
     void contextLoads() {
