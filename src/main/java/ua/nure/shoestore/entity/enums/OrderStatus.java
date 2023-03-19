7a4057d4e255a6634a98c29e9d9c2b2c99eb9d67
Oryna Kasapova <oryna.kasapova@nure.ua>
2023-03-19 13:20:25 +0200

    End entities#3

--- src/main/java/ua/nure/shoestore/entity/enums/OrderStatus.java
+++ src/main/java/ua/nure/shoestore/entity/enums/OrderStatus.java
@@ -1,5 +1,5 @@
 package ua.nure.shoestore.entity.enums;
 
 public enum OrderStatus {
-    PROCESSING, ACCEPTED, COMPILED, SENT_FOR_SENDING, DELIVERED
+    PROCESSING, ACCEPTED, COMPILED, READY_FOR_SENDING, DELIVERED
 }
