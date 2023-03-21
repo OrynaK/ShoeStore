1e11f69df5ac96dae85a957ce21c36fb931e1142
Sofikoshka <sofiia.kolokolcheva@nure.ua>
2023-03-21 22:17:01 +0100

    компонент навбара+стили(потом почищу)

--- src/main/webapp/my-shoestore/src/App.js
+++ src/main/webapp/my-shoestore/src/App.js
@@ -1,6 +1,7 @@
 
 import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <div className="__navbar">
             <Navbar/>
         </div>
-
+            <RegistrationForm/>
     </div>
   );
 }
