dc4944de32b711bf9b28a2a7ff259f90e0c0445b
Klymenko <alexklimovv2@gmail.com>
2023-03-25 20:53:54 +0200

    added check if user exists

--- src/main/webapp/my-shoestore/src/components/LoginForm/LoginForm.jsx
+++ src/main/webapp/my-shoestore/src/components/LoginForm/LoginForm.jsx
@@ -6,17 +6,33 @@ import React from "react";
 function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [user, setUser] = useState('');
     const [error, setError] = useState('');
 
     function handleSubmit(event) {
         event.preventDefault();
-        // здесь можно добавить логику для проверки введенных данных в базе данных
-        if (email === 'example@mail.com' && password === 'password') {
-            setError('');
-            // здесь можно добавить логику для успешного входа
-        } else {
-            setError('Wrong password or login. Try again');
-        }
+        const param = {email, password};
+        fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(param)
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Network response was not ok.");
+        }).then((data) => {
+            const emailValue = data.email ? data.email.trim() : '';
+            const passwordValue = data.password ? data.password.trim() : '';
+            if (!emailValue || !passwordValue) {
+                setError('Wrong password or login. Try again');
+            } else {
+                setError('All ok. REDIRECTING');
+               //ось тут треба додати в сесію користувача який прийшов в респонсі
+            }
+        }).catch((error) => {
+            console.error("There was a problem with the fetch operation:", error);
+        });
     }
 
     return (
@@ -42,7 +58,7 @@ function LoginForm() {
                         value={password}
                         onChange={event => setPassword(event.target.value)}
                     />
-
+                    <input value={error} />
 
                     <button onClick={handleSubmit} type="submit">Log in</button>
                 </form>
