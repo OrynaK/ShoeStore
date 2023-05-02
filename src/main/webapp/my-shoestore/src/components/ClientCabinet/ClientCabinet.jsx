8b472f9968487673903b9bf477ee69782b6d6320
Sofikoshka <sofiia.kolokolcheva@nure.ua>
2023-05-02 17:17:04 +0200

    хз провсяк випадок коміт зроблю

--- src/main/webapp/my-shoestore/src/components/ClientCabinet/ClientCabinet.jsx
+++ src/main/webapp/my-shoestore/src/components/ClientCabinet/ClientCabinet.jsx
@@ -1,40 +1,80 @@
 import "./ClientCabinet.css"
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useLocation} from "react-router";
 
-function ClientCabinet() {
+ function ClientCabinet() {
 
-    const [user, setUser] = useState(() => {
-        const storedUser = localStorage.getItem('user');
-        return storedUser ? JSON.parse(storedUser) : null;
-    });
+     const [user, setUser] = useState(() => {
+         const storedUser = localStorage.getItem('user');
+         return storedUser ? JSON.parse(storedUser) : null;
+     });
+     const [formData, setFormData] = useState({
+         name: user && user.name,
+         surname: user && user.surname,
+         phoneNumber: user && user.phoneNumber,
+         email: user && user.email,
+     });
+
+     const handleFormSubmit = (event) => {
+         event.preventDefault();
+         // Виконати запит до серверу з оновленими даними користувача
+         // та оновити локальний стейт користувача
+         fetch('http://localhost:8080/clientcabinet', {
+             method: 'PATCH',
+             body: JSON.stringify(formData),
+             headers: {
+                 'Content-Type': 'application/json',
+             },
+         })
+             .then((response) => response.json())
+             .then((data) => {
+                 localStorage.setItem('user', JSON.stringify(data));
+                 setUser(data);
+             })
+             .catch((error) => {
+                 console.error('Error:', error);
+             });
+     };
+
+     const handleInputChange = (event) => {
+         const {name, value} = event.target;
+         setFormData((formData) => ({
+             ...formData,
+             [name]: value,
+         }));
+     };
+
+     return (
+         <div className="cabinet">
+             <h1 className="cabinet-header">Особистий кабінет</h1>
+             <h2 className="cabinet-data">Особисті данні</h2>
+                 <div className="cabinet-form">
+                     <span className="cabinet-form-span">{user && user.name}</span>
+                     <span className="cabinet-form-span">{user && user.surname}</span>
+                     <span className="cabinet-form-span">Номер телефону</span>
+                     <span className="cabinet-form-span">
+          <em>{user && user.phoneNumber}</em>
+        </span>
+                     <span className="cabinet-form-span">Email</span>
+                     <span className="cabinet-form-span">{user && user.email}</span>
+                     <span className="cabinet-form-span">Роль</span>
+                     <span className="cabinet-form-span">
+          <em>{user && user.role}</em>
+        </span>
+                     <button className="cabinet-form-btn">Змiнити даннi</button>
+                 </div>
+             <div className="cabinet-basket">
+                 <h2 className="cabinet-basket-header">Кошик</h2>
+                 <div className="basket-form">
+                     <h3 className="basket-form-header">Кошик пустий!</h3>
+                 </div>
+             </div>
+         </div>
+     );
+ }
+
+     export default ClientCabinet;
 
 
-    return (
-        <div className="cabinet">
-            <h1 className="cabinet-header">Особистий кабінет</h1>
-            <h2 className="cabinet-data">Особисті данні</h2>
-            <h2 className="cabinet-basket-header">Кошик</h2>
-        <div className="cabinet-form">
-            <span className="cabinet-form-span">{user && user.name}</span>
-            <span className="cabinet-form-span">{user && user.surname}</span>
-            <span className="cabinet-form-span">Номер телефону</span>
-            <span className="cabinet-form-span"><em>{user && user.phoneNumber}</em></span>
-            <span className="cabinet-form-span">Email</span>
-            <span className="cabinet-form-span">{user && user.email}</span>
-            <span className="cabinet-form-span">Роль</span>
-            <span className="cabinet-form-span"><em>{user && user.role}</em></span>
-            <button className="cabinet-form-btn">Змiнити даннi</button>
-        </div>
-            <div className="cabinet-basket">
-                <div className="basket-form">
-                        <h3 className="basket-form-header">Кошик пустий!</h3>
-                </div>
 
-            </div>
-            </div>
 
-    )
-}
-
-export default ClientCabinet;
