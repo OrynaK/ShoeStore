41200b1c0ebf64efdad66fbe73e2241bc7d72d5f
Sofikoshka <sofiia.kolokolcheva@nure.ua>
2023-05-25 21:54:30 +0200

    курва

--- src/main/webapp/my-shoestore/src/components/ShoePage/ShoePage.jsx
+++ src/main/webapp/my-shoestore/src/components/ShoePage/ShoePage.jsx
@@ -1,45 +1,44 @@
 import React, {useEffect, useState} from "react";
 import "./ShoePage.css";
 import sneaker from "./../../assets/sneaker.png"
-import {useLocation} from "react-router";
+import { useLocation, useParams } from "react-router";
+
 function ShoePage() {
-
-    const location = useLocation();
-    const { id, name, price, image} = location.state || {};
-    const [shoesData, setShoesData] = useState([]);
-
+    const { id, name, price, image } = useLocation().state || {};
+    const [shoeSizes, setShoeSizes] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:8080/showShoePage?name=${name}`)
             .then((response) => response.json())
-            .then((data) => setShoesData(data))
+            .then((data) => {
+                // Отримано дані з бекенду, обробка та встановлення у стан компонента
+                const sizes = data.map((shoe) => shoe.size);
+                setShoeSizes(sizes);
+            })
             .catch((error) => {
                 console.error("Error:", error);
             });
+    }, [name]);
 
-    }, []);
     return (
-
         <div className="shoe-page">
             <h2 className="shoe-page-header">{name}</h2>
             <div className="shoe-page-form">
-                <img src={sneaker} alt="sneaker"/>
+                <img src={sneaker} alt="sneaker" />
                 <div className="shoe-page-form-properties">
-                    <div  className="shoe-page-form-sizes">
-                        {shoesData.map(shoe => (
-                            <span key={shoe.id} className="shoe-page-form-size">{shoe.size}</span>
+                    <div className="shoe-page-form-sizes">
+                        {shoeSizes.map((size) => (
+                            <span key={size} className="shoe-page-form-size">{size}</span>
                         ))}
-
                     </div>
-
                     <span className="shoe-page-form-price"><strong>{price}$</strong></span>
                 </div>
-
                 <button className="shoe-page-form-btn">Додати до кошика</button>
             </div>
         </div>
-
-    )
+    );
 }
 
 export default ShoePage;
+
+
