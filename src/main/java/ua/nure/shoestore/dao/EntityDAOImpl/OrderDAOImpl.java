d7ca8ca3ed8ca195d69c1b73da5bb419085e5037
Oksana Vykhryst <ksysha.vykhryst@gmail.com>
2023-05-25 08:51:27 +0300

    додала метод create в ордер дао

--- src/main/java/ua/nure/shoestore/dao/EntityDAOImpl/OrderDAOImpl.java
+++ src/main/java/ua/nure/shoestore/dao/EntityDAOImpl/OrderDAOImpl.java
@@ -4,18 +4,16 @@ import ua.nure.shoestore.dao.ConnectionManager;
 import ua.nure.shoestore.dao.DAOConfig;
 import ua.nure.shoestore.dao.EntityDAO.OrderDAO;
 import ua.nure.shoestore.entity.Order;
-import ua.nure.shoestore.entity.OrderShoe;
+import ua.nure.shoestore.entity.ShoeOrder;
+import ua.nure.shoestore.entity.enums.Role;
 
-import java.math.BigDecimal;
 import java.sql.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class OrderDAOImpl implements OrderDAO {
+    private static final String INSERT_ORDER = "INSERT INTO `order` (address_id, datetime, status) VALUES (?, DEFAULT, DEFAULT)";
+    private static final String INSERT_SHOES_ORDER = "INSERT INTO `shoe_order` (order_id, shoe_id, price, amount) VALUES (?, ?, ?, ?)";
+    private static final String INSERT_ORDER_USER = "INSERT INTO `user_order` (order_id, user_id, description, datetime) VALUES (?, ?, DEFAULT, DEFAULT)";
     private final ConnectionManager connectionManager;
-    private static final String INSERT_ORDER = "INSERT INTO `order` (date, time, status, address_id) VALUES (?, ?, DEFAULT, ?)";
-    private static final String INSERT_SHOES_ORDER = "INSERT INTO `shoes_order` (order_id, shoe_id, shoes_amount, shoe_price) VALUES (?, ?, ?, ?)";
-    private static final String INSERT_ORDER_USER = "INSERT INTO `order_user` (order_id, user_id) VALUES (?, ?)";
 
     public OrderDAOImpl(DAOConfig config) {
         connectionManager = new ConnectionManager(config);
@@ -27,18 +25,24 @@ public class OrderDAOImpl implements OrderDAO {
         PreparedStatement st = null;
         try {
             conn = connectionManager.getConnection(false);
-            st = conn.prepareStatement(INSERT_ORDER, Statement.RETURN_GENERATED_KEYS);
-            setOrder(order, st);
+            st = conn.prepareStatement(INSERT_ORDER, Statement.RETURN_GENERATED_KEYS); // insert order
+            st.setLong(1, order.getAddress().getId());
             st.executeUpdate();
             ResultSet generatedKeys = st.getGeneratedKeys();
 
-            st = conn.prepareStatement(INSERT_SHOES_ORDER);
-            setShoesOrder(order, st, generatedKeys);
+            st = conn.prepareStatement(INSERT_SHOES_ORDER); // insert shoes
+            if (generatedKeys.next()) { // if we have order id
+                order.setOrderId(generatedKeys.getLong(1));
+                for (ShoeOrder shoeOrder : order.getShoesInOrder()) {
+                    setShoeOrder(order.getOrderId(), shoeOrder, st);
+                    st.addBatch();
+                }
+                st.executeBatch();
 
-            st = conn.prepareStatement(INSERT_ORDER_USER);
-            setUsers(order, st);
-            st.executeBatch();
-
+                st = conn.prepareStatement(INSERT_ORDER_USER); // insert CLIENT
+                setClient(order, st);
+                st.executeUpdate();
+            }
             conn.commit();
         } catch (Exception e) {
             ConnectionManager.rollback(conn);
@@ -48,66 +52,17 @@ public class OrderDAOImpl implements OrderDAO {
         }
     }
 
-    private static void setOrder(Order order, PreparedStatement st) throws SQLException {
+    private static void setShoeOrder(long orderId, ShoeOrder shoeOrder, PreparedStatement st) throws SQLException {
         int k = 0;
-        st.setDate(++k, java.sql.Date.valueOf(order.getDate()));
-        st.setTime(++k, java.sql.Time.valueOf(order.getTime()));
-        st.setLong(++k, order.getAddress().getAddressId());
+        st.setLong(++k, orderId);
+        st.setLong(++k, shoeOrder.getShoeId());
+        st.setBigDecimal(++k, shoeOrder.getPrice());
+        st.setInt(++k, shoeOrder.getAmount());
     }
 
-    private static void setShoesOrder(Order order, PreparedStatement st, ResultSet generatedKeys) throws SQLException {
-        int k;
-        while (generatedKeys.next()) {
-            order.setOrderId(generatedKeys.getLong(1));
-            for (OrderShoe orderShoe : order.getShoesInOrder()) {
-                k = 0;
-                st.setLong(++k, order.getOrderId());
-                st.setLong(++k, orderShoe.getShoeId());
-                st.setInt(++k, orderShoe.getAmount());
-                st.setBigDecimal(++k, orderShoe.getPrice());
-                st.addBatch();
-            }
-            st.executeBatch();
-        }
-    }
-
-    private static void setUsers(Order order, PreparedStatement st) throws SQLException {
+    private static void setClient(Order order, PreparedStatement st) throws SQLException {
         st.setLong(1, order.getOrderId());
-        st.setLong(2, order.getClientId());
-        st.addBatch();
-
-        st.setLong(1, order.getOrderId());
-        st.setLong(2, order.getAdminId());
-        st.addBatch();
-
-        st.setLong(1, order.getOrderId());
-        st.setLong(2, order.getPackerId());
-        st.addBatch();
-
-        st.setLong(1, order.getOrderId());
-        st.setLong(2, order.getWarehouseId());
-        st.addBatch();
-
-        st.setLong(1, order.getOrderId());
-        st.setLong(2, order.getCourierId());
-        st.addBatch();
-    }
-
-    public static void main(String[] args) {
-        // test
-        OrderDAO orderDAO = new OrderDAOImpl(new DAOConfig("MySQL", "jdbc:mysql://localhost:3306/shoe_store?sslMode=DISABLED&serverTimezone=UTC", "root", "root"));
-        Order order = new Order();
-        order.setClientId(14);
-        order.setAdminId(15);
-        order.setPackerId(16);
-        order.setWarehouseId(17);
-        order.setCourierId(18);
-        order.setDate(LocalDate.now());
-        order.setTime(LocalTime.now());
-        order.setAddress(new AddressDAOImpl(new DAOConfig("MySQL", "jdbc:mysql://localhost:3306/shoe_store?sslMode=DISABLED&serverTimezone=UTC", "root", "root")).getById(1));
-        order.getShoesInOrder().add(new OrderShoe(37, 2, BigDecimal.valueOf(1000)));
-        order.getShoesInOrder().add(new OrderShoe(38, 1, BigDecimal.valueOf(2000)));
-        orderDAO.create(order);
+        st.setLong(2, order.getUsersInOrder().get(Role.CLIENT).getUserId());
     }
 
     @Override
@@ -120,4 +75,14 @@ public class OrderDAOImpl implements OrderDAO {
         throw new UnsupportedOperationException();
     }
 
+/*    public static void main(String[] args) throws SQLException {
+        // test
+        OrderDAOImpl orderDAO = new OrderDAOImpl(new DAOConfig("MySQL", "jdbc:mysql://localhost:3306/shoe_store?sslMode=DISABLED&serverTimezone=UTC", "root", "root"));
+        Order order = new Order();
+        order.putUser(Role.CLIENT, new UserOrder(20));
+        order.setAddress(new AddressDAOImpl(new DAOConfig("MySQL", "jdbc:mysql://localhost:3306/shoe_store?sslMode=DISABLED&serverTimezone=UTC", "root", "root")).getById(1));
+        order.getShoesInOrder().add(new ShoeOrder(37, BigDecimal.valueOf(1000), 2));
+        order.getShoesInOrder().add(new ShoeOrder(38, BigDecimal.valueOf(2000), 1));
+        orderDAO.create(order);
+    }*/
 }
--- src/main/java/ua/nure/shoestore/entity/Order.java
+++ src/main/java/ua/nure/shoestore/entity/Order.java
@@ -3,30 +3,35 @@ package ua.nure.shoestore.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ua.nure.shoestore.entity.enums.OrderStatus;
+import ua.nure.shoestore.entity.enums.Role;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Data
 @AllArgsConstructor
 public class Order {
     private long orderId;
-    private long clientId;
-    private long adminId;
-    private long packerId;
-    private long warehouseId;
-    private long courierId;
+    private Map<Role, UserOrder> usersInOrder;
     private LocalDate date;
     private LocalTime time;
     private OrderStatus status;
     private Address address;
-    private List<OrderShoe> shoesInOrder;
-    private BigDecimal cost;
+    private List<ShoeOrder> shoesInOrder;
+    private BigDecimal totalCost;
 
     public Order(){
         shoesInOrder = new ArrayList<>();
+        usersInOrder = new EnumMap<>(Role.class);
+    }
+
+    public void addShoe(ShoeOrder shoeOrder){
+        shoesInOrder.add(shoeOrder);
+    }
+
+    public void putUser(Role role, UserOrder userOrder){
+        usersInOrder.put(role, userOrder);
     }
 }
