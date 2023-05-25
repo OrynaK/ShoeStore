e0fdc982ec4c420e9ded28b4a2c4b92da8269f7e
Oksana Vykhryst <ksysha.vykhryst@gmail.com>
2023-05-25 10:02:49 +0300

    refactor

--- src/main/java/ua/nure/shoestore/dao/EntityDAO/AddressDAO.java
+++ src/main/java/ua/nure/shoestore/dao/EntityDAO/AddressDAO.java
@@ -3,7 +3,6 @@ package ua.nure.shoestore.dao.EntityDAO;
 import ua.nure.shoestore.dao.CRUDRepository;
 import ua.nure.shoestore.entity.Address;
 
-import java.util.List;
-
 public interface AddressDAO extends CRUDRepository<Address> {
+
 }
